22a284e27787dd0dc4222a3f51fd39a974f3d05c
sircoolio <ericklein@users.noreply.github.com>
2020-09-11 15:07:43 -0700

    Crash fix
    fixed crash after one main() iteration

--- air_quality.ino
+++ air_quality.ino
@@ -167,16 +167,15 @@ void setup()
       Serial.println(Ethernet.localIP());
     #endif
     Udp.begin(localPort);
-    #ifdef DEBUG
-      Serial.println("waiting for NTP time sync");
-    #endif
-    setSyncInterval(15); // sync every 15 seconds
     setSyncProvider(getNtpTime);
 
-    while(timeStatus()== timeNotSet)
-      ; // wait until the time is set by the sync provider
+    // wait until the time is set by the sync provider
+    while(timeStatus()== timeNotSet);
 
-    Serial.println("Sync successfully");
+    #ifdef DEBUG
+      Serial.print("The NTP time is ");
+      digitalClockDisplay();
+    #endif
   #endif
 
   // serial output data headers
@@ -187,6 +186,9 @@ void setup()
 
 void loop() 
 {
+  // #ifdef DEBUG
+  //   Serial.println("Entering main loop");
+  // #endif
   // delay between sensor readings
   delay(LOG_INTERVAL);
 
@@ -270,13 +272,16 @@ void loop()
 time_t getNtpTime()
 {
   while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
+  #ifdef DEBUG
+    Serial.println("Transmitting NTP Request");
+  #endif
   sendNTPpacket(timeServer);
   uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
+  while (millis() - beginWait < 1500)
+  {
     int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
+    if (size >= NTP_PACKET_SIZE)
+    {
       Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
       unsigned long secsSince1900;
       // convert four bytes starting at location 40 to a long integer
@@ -287,7 +292,9 @@ time_t getNtpTime()
       return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
     }
   }
-  Serial.println("No NTP Response :-(");
+  #ifdef DEBUG
+    Serial.println("No NTP response");
+  #endif
   return 0; // return 0 if unable to get the time
 }
 
@@ -297,18 +304,16 @@ void sendNTPpacket(IPAddress &address)
   // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
   // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  packetBuffer[0] = 0b11100011; // LI, Version, Mode
+  packetBuffer[1] = 0;          // Stratum, or type of clock
+  packetBuffer[2] = 6;          // Polling Interval
+  packetBuffer[3] = 0xEC;       // Peer Clock Precision
   // 8 bytes of zero for Root Delay & Root Dispersion
   packetBuffer[12]  = 49;
   packetBuffer[13]  = 0x4E;
   packetBuffer[14]  = 49;
   packetBuffer[15]  = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:                 
+  // send a packet requesting a timestamp:                 
   Udp.beginPacket(address, 123); //NTP requests are to port 123
   Udp.write(packetBuffer, NTP_PACKET_SIZE);
   Udp.endPacket();
@@ -320,4 +325,27 @@ uint32_t getAbsoluteHumidity(float temperature, float humidity)
   const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
   const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
   return absoluteHumidityScaled;
+}
+
+void digitalClockDisplay()
+{
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print("/");
+  Serial.print(day());
+  Serial.print("/");
+  Serial.println(year()); 
+}
+
+void printDigits(int digits)
+{
+  // utility for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
 }
--- readme.md
+++ readme.md
@@ -48,12 +48,11 @@ Measure and log temperature, humidity, and CO2 levels every 30 minutes
 	- https://www.adafruit.com/product/4003#:~:text=Description%2D-,Description,along%20with%20a%20Feather%20accessory
 	- https://learn.adafruit.com/adafruit-wiz5500-wiznet-ethernet-featherwing/usage
 	- https://www.arduino.cc/en/reference/ethernet
+	- https://store.arduino.cc/usa/arduino-ethernet-rev3-without-poe
 
 ### Issues
 - 083120: Need to add baseline readings for the SGP30 (EPROM, FLASH)
 - 083120: Arduino Ethernet doesn't have enough memory for code + libraries, might even be an SRAM issue b4 that
-- 090620: SGP30 is not being baseline calibrated
-- 090820: time is not correct, sample code is
 
 ### Questions
 - 090820: We are generating humidity, heat index, and absolute humidity?
@@ -63,11 +62,13 @@ Measure and log temperature, humidity, and CO2 levels every 30 minutes
 - 090620: Arduino Ethernet code can't tranverse a DNS fallback list, so if the primary fails (e.g. Pihole crash) it will stop connecting to outside addresses via DNS lookup
 
 ### Feature Requests
-- 083120: Upload data to internet db
+- 083120: Upload data to cloud db
 - 090120: Add screen display support
 - 090620: LED blink encoded error messages for non-DEBUG and while(1) errors
 - 090820: Optimize code
-- 090820: Display NTP time when received in DEBUG
+- 090820: After adding cloud db support, try and backport to Arduino Ethernet board
+- 090820: Switch to Adafruit M0 Datalogger and re-enable SDLOG
+- 091120: Use timedisplay routines for log string
 
 ### Revisions
 - 083120: First version based on merged sample code for sensors, SD. Ethernet code NOT working.
@@ -76,4 +77,9 @@ Measure and log temperature, humidity, and CO2 levels every 30 minutes
 	- Switched to Particle Ethernet Featherwing and Feather M4 Express
 	- Switched to timelib getNtpTime example code
 	- [FR] 090120: Conditional compile for network, SD saves, and display
-	- [FR] 090120: Switch to ARM SoC for memory (post conditionals?)
+	- [FR] 090120: Switch to ARM SoC for +memory (post conditionals?)
+-091120
+	- [I] 090820: Code is only running for one loop -> setSyncInterval(15) locked code on subsequent loops, also not needed
+	- [I] - 090820: time is not correct, sample code is -> byproduct of setSyncInterval(15) issue
+	- [FR] 090820: Display NTP time when received in DEBUG
+	
